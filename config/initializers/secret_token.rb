1d708268cc0a9097b3deeae8d6005f60e1f4e41b
Sara LaFassett <sara@lafassett.com>
2013-11-25 11:04:43 -0800

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpellableWords::Application.config.secret_token = 'fc6ec7f50a85360df5695b05e4e72b7146725099f681a74fed52a576f98c576b50b99ac89e9b307ddf72259d66938a2b2dd60d4b60edd300fa795a07488ef4d3'
